5147e34b3fe6658752a090fa3cc7f2d8bd676b20
310164420 <310164420@YY504237.mshome.net>
2019-08-13 11:10:00 +0200

    further change

--- jbpm-simulation/src/main/java/org/jbpm/simulation/impl/WorkingMemorySimulationRepository.java
+++ jbpm-simulation/src/main/java/org/jbpm/simulation/impl/WorkingMemorySimulationRepository.java
@@ -15,19 +15,32 @@
 
 package org.jbpm.simulation.impl;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.jbpm.simulation.AggregatedSimulationEvent;
 import org.jbpm.simulation.SimulationEvent;
 import org.jbpm.simulation.impl.events.AggregatedActivitySimulationEvent;
+import org.jbpm.simulation.impl.events.AggregatedEndEventSimulationEvent;
 import org.jbpm.simulation.impl.events.AggregatedProcessSimulationEvent;
+import org.jbpm.simulation.impl.events.HumanTaskActivitySimulationEvent;
 import org.kie.api.io.Resource;
 import org.kie.api.io.ResourceType;
 import org.kie.api.runtime.KieSession;
 import org.kie.internal.builder.KnowledgeBuilder;
 import org.kie.internal.builder.KnowledgeBuilderFactory;
 import org.kie.internal.io.ResourceFactory;
+
+
 
 public class WorkingMemorySimulationRepository extends InMemorySimulationRepository {
 
@@ -51,7 +64,7 @@ public class WorkingMemorySimulationRepository extends InMemorySimulationReposit
         KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
         
         for (Resource path : rules) {
-        
+        	System.out.println(String.format("adding resource rule: %s", path.toString()));
             kbuilder.add(path, ResourceType.DRL);
         }
         if (kbuilder.hasErrors()) {
@@ -78,7 +91,7 @@ public class WorkingMemorySimulationRepository extends InMemorySimulationReposit
         KnowledgeBuilder kbuilder = KnowledgeBuilderFactory.newKnowledgeBuilder();
         
         for (String path : rules) {
-        
+        	System.out.println(String.format("adding rule: %s", path));
             kbuilder.add(ResourceFactory.newClassPathResource(path), ResourceType.DRL);
         }
         if (kbuilder.hasErrors()) {
@@ -97,13 +110,16 @@ public class WorkingMemorySimulationRepository extends InMemorySimulationReposit
     
     public void storeEvent(SimulationEvent event) {
         super.storeEvent(event);
+        System.out.println(String.format("storing event in WMSR. session: %s, event: %s", ksession.toString(),event.toString()));
         ksession.insert(event);
         if (fireRulesOnStore) {
+        	System.out.println("firing rules when storing event");
             ksession.fireAllRules();
         }
     }
 
     public void fireAllRules() {
+    	System.out.println("firing rules by calling");
         ksession.fireAllRules();
     }
     
@@ -121,6 +137,67 @@ public class WorkingMemorySimulationRepository extends InMemorySimulationReposit
 
     @Override
     public void close() {
+    	//Hongchao, outputs events
+    	System.out.println("outputing event log: ");
+    	for (SimulationEvent e : this.getEvents()) {
+    		System.out.println(e.toString());
+    	}
+    	DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+    	try (
+                BufferedWriter writer = Files.newBufferedWriter(Paths.get("./simulation_log.csv"));
+
+                CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.DEFAULT
+                        .withHeader("process-instance-id", "event-id","event-name", "event-type", "startTime","endTime"));
+    			
+            ) {
+    		for (SimulationEvent e : this.getEvents()) {
+    			String event_name="";
+    			String event_id="";
+    			String event_type=e.getType();
+    			if (event_type ==null) {
+    				event_type="unknown";
+    			}
+    			String startTime="";
+    			String endTime = "";
+    			boolean output=true;
+    			switch (event_type) {
+    			case "userTask":
+    				HumanTaskActivitySimulationEvent hte = (HumanTaskActivitySimulationEvent)e;
+    				event_name = hte.getActivityName();
+    				event_id = hte.getActivityId();
+    				startTime=dateFormat.format(new Date(e.getStartTime()));
+    				endTime = dateFormat.format(new Date(e.getEndTime()));
+    				break;
+    			case "startEvent":
+    				event_name="startEvent";
+    				startTime=dateFormat.format(new Date(e.getStartTime()));
+    				endTime=startTime;
+    				break;
+    			case "endEvent":
+    				event_name="endEvent";
+    				endTime = dateFormat.format(new Date(e.getEndTime()));
+    				startTime=endTime;
+    				break;
+    			case "process-instance":
+    				output=false;
+    				break;
+    			default:
+    				output=false;
+    				break;
+    			}
+        		if(output) {
+        			csvPrinter.printRecord(e.getProcessInstanceId(), event_id,event_name, event_type, startTime,endTime);
+        		}
+        	}
+                
+                //csvPrinter.printRecord("2", "Satya Nadella", "CEO", "Microsoft");
+
+                csvPrinter.flush();     
+                csvPrinter.close();
+            } catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
         super.close();
         this.ksession.dispose();
     }
